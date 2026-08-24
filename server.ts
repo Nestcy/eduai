9a6affc9d2cfe0809fa513acb98dd5c0228e9ae2
Ernest Zimba <nestcy770@gmail.com>
2026-08-24 14:29:45 +0200

    deploy capability

--- server.ts
+++ server.ts
@@ -74,7 +74,7 @@ const activeIngestionJobs = new Map<string, IngestionJob>();
 
 // Initialize Express
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
